aa869471ac05252935f66a23c37263e55ad2a72c
lucasFR2 <lucasfirminor6602@gmail.com>
2026-05-08 12:24:44 -0300

    corrigindo controllers, models e rotas que foram apagadas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductController;   // Adicione isso
+use App\Http\Controllers\InventoryController; // Adicione isso
+use App\Http\Controllers\SupplierController;  // Adicione isso
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
@@ -15,6 +18,11 @@ Route::middleware('auth')->group(function () {
     Route::post('/logout',    [AuthController::class, 'logout'])->name('logout');
     Route::get('/dashboard',  [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/admin',      fn() => 'Área Admin')->middleware('role:admin')->name('admin');
+
+    // ADICIONE ESTAS TRÊS LINHAS ABAIXO:
+    Route::resource('products', ProductController::class);
+    Route::resource('inventory', InventoryController::class);
+    Route::resource('suppliers', SupplierController::class);
 });
 
 Route::get('/', fn() => redirect()->route('login'));
